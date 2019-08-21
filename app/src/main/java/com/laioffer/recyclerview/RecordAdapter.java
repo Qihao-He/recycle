5a39e40f5a845c836510af6d9ec0733adea383e2
Qihao He <qi.he@maine.edu>
2019-08-20 19:07:14 -0700

    first commit

---
+++ app/src/main/java/com/laioffer/recyclerview/RecordAdapter.java
@@ -0,0 +1,61 @@
+package com.laioffer.recyclerview;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class RecordAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private List<Item> items;
+    private Context context;
+
+    public RecordAdapter(Context context, List<Item> items) {
+        this.context = context;
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        //TODO
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        //TODO
+        return 0;
+    }
+
+    /* viewholder example */
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView name;
+        TextView productName;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            name = itemView.findViewById(R.id.company_name);
+            productName = itemView.findViewById(R.id.product_name);
+        }
+    }
+
+    // TODO we need different viewholder
+
+
+}
